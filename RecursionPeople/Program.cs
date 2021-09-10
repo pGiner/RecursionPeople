5c1a0a5247b6c6b085a52b519da5ac27fd0f88c8
Pablo Giner Cebrián <pginer@hiberus.com>
2021-09-10 11:14:22 +0200

    Agregar archivos de proyecto.

---
+++ RecursionPeople/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecursionPeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> people= new List<String>();
+            people = Recursivity.Saludar("Clara", "Juan");
+            people.ForEach(Console.WriteLine);
+
+        }
+    }
+}
---
+++ RecursionPeople/Recursivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecursionPeople
+{
+    class Recursivity
+    {
+        static readonly String[,] ArrayPersonas = { {"Juan", "Juan", "Fernando", "Maria" }, {"Pedro", "Fernando", "Maria", "Clara"} };
+        static List<String> people = new List<string>();
+        public static List<String> Saludar(String saludado, String saludador)
+        {
+            if (!people.Contains(saludado))
+            {
+                people.Add(saludado);
+            }
+            
+            if(saludado == saludador)
+            {
+                if (!people.Contains(saludado)) 
+                {
+                    people.Add(saludado);
+                }
+               
+                return people;
+            }
+
+            for (int x = 0; x < ArrayPersonas.GetLength(0); x += 1)
+            {
+                for (int y = 0; y < ArrayPersonas.GetLength(1); y += 1)
+                {
+                    //X=0 PRIMERA COLUMNA, X=1 SEGUNDA COLUMNA
+                    if(saludado.Equals(ArrayPersonas[1, y]))
+                    {
+                        if (!people.Contains(ArrayPersonas[0, y]))
+                        {
+                            people.Add(ArrayPersonas[0, y]);
+                        }
+                        Saludar(ArrayPersonas[0, y], saludador);                        
+                    }
+                }
+            }
+            return people;
+        }
+    }
+}
